9d5d3e22a486c781d614000b3cf11f561d05b68f
Paul Bagyenda <bagyenda@dsmagic.com>
2020-10-27 11:13:28 +0300

    - misc refactoring

---
+++ src/io/njiwa/common/rest/RestWSEntryPoint.java
@@ -0,0 +1,23 @@
+/*
+ * Njiwa Open Source Embedded M2M UICC Remote Subscription Manager
+ * 
+ * 
+ * Copyright (C) 2019 - , Digital Solutions Ltd. - http://www.dsmagic.com
+ *
+ * Njiwa Dev <dev@njiwa.io>
+ * 
+ * This program is free software, distributed under the terms of
+ * the GNU General Public License.
+ */ 
+
+package io.njiwa.common.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Created by bagyenda on 29/05/2017.
+ */
+@ApplicationPath("/rest")
+public class RestWSEntryPoint extends Application {
+}
